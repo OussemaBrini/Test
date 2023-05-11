e0a07b5581399df3f672f82322cfcbab3991b4a6
oussema <93262073+OussemaBrini@users.noreply.github.com>
2023-05-11 12:20:51 +0200

    Update ActionController .cs

--- Test/Controllers/ActionController .cs
+++ Test/Controllers/ActionController .cs
@@ -1,11 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Test.Controllers
+[ApiController]
+[Route("[controller]")]
+public class ActionController : ControllerBase
 {
-    public class ActionController
+    [HttpGet]
+    public ActionResult<Action> Get()
     {
+        var actions = new[] { "AMZN", "CACC", "EQIX", "GOOG", "ORLY", "ULTA" };
+        var prices = new[,]
+        {
+            { 12.81, 11.09, 12.11, 10.93, 9.83, 8.14 },
+            { 10.34, 10.56, 10.14, 12.17, 13.1, 11.22 },
+            { 11.53, 10.67, 10.42, 11.88, 11.77, 10.21 }
+        };
+
+        var avgPrices = new double[actions.Length];
+        for (int i = 0; i < actions.Length; i++)
+        {
+            double sum = 0;
+            for (int j = 0; j < prices.GetLength(1); j++)
+            {
+                sum += prices[i, j];
+            }
+            avgPrices[i] = sum / prices.GetLength(1);
+        }
+
+        int maxIndex = avgPrices.ToList().IndexOf(avgPrices.Max());
+
+        return new Action
+        {
+            Name = actions[maxIndex],
+            AvgPrice = avgPrices[maxIndex]
+        };
     }
+}
+
+public class Action
+{
+    public string Name { get; set; }
+    public double AvgPrice { get; set; }
 }
